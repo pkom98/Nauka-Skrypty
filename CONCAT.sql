35601bc92f18e333c1ce2fc93a5d0be7ec9a73a5
Pawel <you@example.com>
2022-11-17 16:55:14 +0100

    First change to CONCAT

--- CONCAT.sql
+++ CONCAT.sql
@@ -1,1 +1,1 @@
-SELECT concat(kategoria, ': ', model, ' ', 'CENA', ' ',cena, ': ', 'zł') AS PRODUKT FROM produkty
+SELECT concat(kategoria, ': ', model, ' ', 'CENA', ' ',cena, ': ', 'zł') AS PRODUKT FROM produkty;
